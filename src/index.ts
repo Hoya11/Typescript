b84f5286ec9df8dd3b6811a5a50fdb2bf103512f
Hoya11 <88309582+Hoya11@users.noreply.github.com>
2022-06-19 00:00:13 +0900

    [feat] Ts-chain

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import crypto from "crypto";
+import { runInThisContext } from "vm";
 
 interface BlockShape {
   hash: string;
@@ -18,5 +19,38 @@ class Block implements BlockShape {
   }
   static calcucateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("첫번째");
+blockchain.addBlock("두번째");
+blockchain.addBlock("세번째");
+
+blockchain.getBlocks().push(new Block("xxx", 12212, "AAA"));
+
+console.log(blockchain.getBlocks());
